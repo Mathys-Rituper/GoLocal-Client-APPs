b830a41766e2834c058d2c536c8cef9be03c3e3d
Alexandre Artaud <alexandre.artaud@etu.univ-grenoble-alpes.fr>
2021-05-21 15:22:02 +0200

    fix of account golocal & client golocal

--- apps/account-golocal/src/Layouts/Header/Header.js
+++ apps/account-golocal/src/Layouts/Header/Header.js
@@ -66,7 +66,7 @@ export default function Header() {
         }
     ];
     function goHome(){
-        window.location.href="./";
+        window.location.href="https://localhost:3001/";
     }
     return(
         <div style={{width:"100%"}}>

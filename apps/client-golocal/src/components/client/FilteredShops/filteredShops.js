50a2ed17bf8ab7b638b9b725e0b15ea9c50bf85d
Alexandre Artaud <alexandre.artaud@etu.univ-grenoble-alpes.fr>
2021-05-23 16:27:42 +0200

    filteredShops.js update

--- apps/client-golocal/src/components/client/FilteredShops/filteredShops.js
+++ apps/client-golocal/src/components/client/FilteredShops/filteredShops.js
@@ -7,15 +7,32 @@ import 'primeflex/primeflex.css';
 import './filteredShops.css'
 import {ScrollPanel} from "primereact/scrollpanel";
 import FilteredProductCard from "./filteredProductCard";
+import {Slider} from "primereact/slider";
+import {SelectButton} from "primereact/selectbutton";
 
 
 
 export default function FilteredShops() {
-
+    const [value, setValue] = useState([0,200]);
+    const options = ['Croissant', 'Décroissant'];
+    const [value1, setValue1] = useState('Croissant');
+    console.log(value1)
+    if (value1 === null){
+        setValue1(options[0]);
+    }
     return (
         <div className="body p-grid">
-            <div className="customSidebar p-col-fixed" style={{ width: '20%'}}>
-                Fixed
+            <div className="customSidebar p-col-fixed p-grid p-dir-col">
+                <span className="filter-title">FILTRES</span>
+                <div className="filters">
+                    <h3>Prix : {value[0]} - {value[1]} €</h3>
+                    <Slider value={value} onChange={(e) => setValue(e.value)} range  max={2000}/>
+                    <h3 style={{marginTop:"10%"}}>Ordre Triage</h3>
+                    <SelectButton value={value1} options={options} onChange={(e) => {
+                        setValue1(e.value);
+                    }} />
+                </div>
+
             </div>
             <div className="filteredContent p-col">
                 <ScrollPanel style={{ width: '100%', height: '790px'}} className="custombar1">
